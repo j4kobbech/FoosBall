91fb05c14c3b7074b7cf1271a2f073d53106ca05
j4kobbech <jbe@trustpilot.com>
2014-08-09 21:53:52 +0200

    better formatting of route configuration

--- FoosBall/js/app/foosball.app.js
+++ FoosBall/js/app/foosball.app.js
@@ -3,16 +3,42 @@
 // Configuration & Routing
 FoosBall.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/', { redirectTo: '/players' })
-        .when('/login', { templateUrl: '/partials/login.html', controller: 'LoginController' })
-        .when('/signup', { templateUrl: '/partials/signup.html', controller: 'SignupController' })
-        .when('/stats', { templateUrl: '/partials/stats.html', controller: 'StatsController' })
-        .when('/playerstats', { templateUrl: '/partials/stats-player.html', controller: 'PlayerStatsController' })
-        .when('/advancedstats', { templateUrl: '/partials/advanced-stats.html', controller: 'AdvancedStatsController' })
-        .when('/matches', { templateUrl: '/partials/matches.html', controller: 'MatchesController' })
-        .when('/players', { templateUrl: '/partials/players.html', controller: 'PlayersController' })
-        .when('/user-profile', { templateUrl: '/partials/user-profile.html', controller: 'UserController' })
-        .when('/sign-up', { templateUrl: '/partials/user-register.html', controller: 'UserController' })
-        .when('/admin', { templateUrl: '/partials/admin.html' })
-        .otherwise({ redirectTo: '/' });
+        .when('/', {
+             redirectTo: '/players'
+        })
+        .when('/login', {
+            templateUrl: '/partials/login.html',
+            controller: 'LoginController'
+        })
+        .when('/signup', {
+            templateUrl: '/partials/signup.html',
+            controller: 'SignupController'
+        })
+        .when('/stats', {
+            templateUrl: '/partials/stats.html',
+            controller: 'StatsController'
+        })
+        .when('/playerstats', {
+            templateUrl: '/partials/stats-player.html',
+            controller: 'PlayerStatsController'
+        })
+        .when('/matches', {
+            templateUrl: '/partials/matches.html',
+            controller: 'MatchesController'
+        })
+        .when('/players', {
+            templateUrl: '/partials/players.html',
+            controller: 'PlayersController'
+        })
+        .when('/user-profile', {
+            templateUrl: '/partials/user-profile.html',
+            controller: 'UserController'
+        })
+        .when('/admin', {
+            templateUrl: '/partials/admin.html',
+            controller: 'AdminController'
+        })
+        .otherwise({
+             redirectTo: '/'
+        });
 }]);

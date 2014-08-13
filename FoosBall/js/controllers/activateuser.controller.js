bf4640212e8e00954bb192e865409515c2b8a072
j4kobbech <jbe@trustpilot.com>
2014-08-13 07:17:51 +0200

    Add view and controller to activate user

---
+++ FoosBall/js/controllers/activateuser.controller.js
@@ -0,0 +1,11 @@
+﻿FoosBall.controller('ActivateUserController', ['$scope', '$location', 'api', function ($scope, $location, api) {
+
+    var queryString = $location.search();
+
+    var promiseOfUserActivation = api.activateUser(queryString.userId, queryString.token);
+
+    promiseOfUserActivation.then(function(response) {
+        console.log(response);
+    });
+
+}]);
